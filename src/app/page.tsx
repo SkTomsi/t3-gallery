2a239694947bd09831a33a197324bbb8bee9bfff
sktomsi <tomcy48@gmail.com>
2024-04-15 21:43:22 +0530

    get the base ui done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,28 @@
-import Link from "next/link";
-
 export default function HomePage() {
+  const mockUrls = [
+    "https://utfs.io/f/9b1a366d-7799-4fb4-ba12-545cdbe66610-w731gh.png",
+    "https://utfs.io/f/9b1a366d-7799-4fb4-ba12-545cdbe66610-w731gh.png",
+    "https://utfs.io/f/9b1a366d-7799-4fb4-ba12-545cdbe66610-w731gh.png",
+    "https://utfs.io/f/9b1a366d-7799-4fb4-ba12-545cdbe66610-w731gh.png",
+    "https://utfs.io/f/9b1a366d-7799-4fb4-ba12-545cdbe66610-w731gh.png",
+    "https://utfs.io/f/9b1a366d-7799-4fb4-ba12-545cdbe66610-w731gh.png",
+    "https://utfs.io/f/9b1a366d-7799-4fb4-ba12-545cdbe66610-w731gh.png",
+  ];
+
+  const mockImages = mockUrls.map((url, index) => ({
+    id: index + 1,
+    url,
+  }));
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages].map((image) => (
+          <div className="" key={image.id}>
+            <img src={image.url} className="w-48" />
+          </div>
+        ))}
+      </div>
       T3 Gallery (in progress)
     </main>
   );
