4eb8bece8e7269a393a720ca0c4ac42f686847b7
sktomsi <tomcy48@gmail.com>
2024-04-21 18:02:15 +0530

    add modal and parallel route

--- src/app/img/[id]/page.tsx
+++ src/app/img/[id]/page.tsx
@@ -1,4 +1,4 @@
-import { getImage } from '~/server/queries';
+import FullPageImageView from '~/components/full-image-page';
 
 export default async function PhotoPage({
   params: { id },
@@ -7,11 +7,6 @@ export default async function PhotoPage({
 }) {
   const idAsNumber = Number(id);
   if (Number.isNaN(idAsNumber)) throw new Error('Invalid photo id');
-  const image = await getImage(idAsNumber);
 
-  return (
-    <div>
-      <img src={image.url} className="w-96" />
-    </div>
-  );
+  return <FullPageImageView id={idAsNumber} />;
 }
